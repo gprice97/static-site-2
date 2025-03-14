c0239673a0ad24f329cff45334aed17a0b7be569
Gunnar Price <Gunnar.Price@Pentair.com>
2025-03-14 14:46:07 -0400

    NodeSplitter updates

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from htmlnode import HTMLNode
 from leafnode import LeafNode
 from textnode import TextNode, TextType, text_node_to_html_node
+from nodesplitter import NodeSplitter
 
 
 def main():
+    splitter = NodeSplitter
     my_text_node = TextNode("This is my Text", TextType.NORMAL, "www.google.com")
     print(my_text_node.__repr__())
     my_html_node = HTMLNode("a", "Click here", [], {"href": "https://boot.dev"})
@@ -12,6 +14,9 @@ def main():
     print(to_html_test)
     node = TextNode("This is a text node", TextType.NORMAL)
     print(text_node_to_html_node(node))
+    node_1 = TextNode("This is text with **BOLD** written all over it", TextType.NORMAL)
+    new_nodes = splitter.split_nodes_delimiter([node_1], "**", TextType.BOLD)
+    print(new_nodes)
 
 
 if __name__ == "__main__":
--- src/nodesplitter.py
+++ src/nodesplitter.py
@@ -7,30 +7,15 @@ class NodeSplitter:
         new_nodes = []
         for node in old_nodes:
             if delimiter not in node.text:
-                raise Exception("Delimiter does not match provided text")
-            if node.text_type is TextType.NORMAL:
-                new_nodes.append(node)
-            elif node.text_type is TextType.BOLD:
-                text_list = node.text.split(delimiter)
-                new_nodes.extend(
-                    [TextNode(text_list[0], TextType.NORMAL),
-                     TextNode(text_list[1], text_type),
-                     TextNode(text_list[2], TextType.NORMAL)
-                     ])
-            elif node.text_type is TextType.ITALIC:
-                text_list = node.text.split(delimiter)
-                new_nodes.extend(
-                    [TextNode(text_list[0], TextType.NORMAL),
-                     TextNode(text_list[1], text_type),
-                     TextNode(text_list[2], TextType.NORMAL)
-                     ])
-            elif node.text_type is TextType.CODE:
-                text_list = node.text.split(delimiter)
-                new_nodes.extend(
-                    [TextNode(text_list[0], TextType.NORMAL),
-                     TextNode(text_list[1], text_type),
-                     TextNode(text_list[2], TextType.NORMAL)
-                     ])
+                new_nodes.extend(TextNode(node.text, node.text_type))
+            node_text_list = node.text.split(delimiter)
+            if len(node_text_list) % 2 != 1:
+                raise Exception("There is a delimiter present without it's matching pair")
+            for index, node_text in enumerate(node_text_list):
+                if index % 2 == 1:
+                    new_nodes.append(TextNode(node_text, text_type))
+                else:
+                    new_nodes.append(TextNode(node_text, node.text_type))
         return new_nodes
 
 
